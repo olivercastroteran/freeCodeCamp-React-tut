16c6123f9f97a55b3b8f8a8660779490e5fa2e78
oliver <olivercastroteran@gmail.com>
2022-02-25 16:00:07 -0400

    rendering text over meme img from form input

--- p-3/src/components/Meme.js
+++ p-3/src/components/Meme.js
@@ -2,25 +2,64 @@ import { useState } from 'react';
 import { memesData } from './memesData';
 
 const Meme = () => {
-  const [memeImg, setMemeImg] = useState('');
+  const [allMemeImgs] = useState(memesData);
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    imgUrl: 'https://i.imgflip.com/30b1gx.jpg',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMeme((prevMeme) => ({
+      ...meme,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const memesArr = memesData.data.memes;
+    const memesArr = allMemeImgs.data.memes;
     const randomNum = Math.floor(Math.random() * memesArr.length);
-    const url = memesArr[randomNum].url;
-    setMemeImg(url);
+
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        imgUrl: memesArr[randomNum].url,
+      };
+    });
+    console.log(meme);
   };
 
   return (
     <main>
       <form className="form" onSubmit={handleSubmit}>
-        <input type="text" className="form--input" placeholder="Top Text" />
-        <input type="text" className="form--input" placeholder="Bottom Text" />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top Text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom Text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--btn">Get a new meme image 🪟</button>
       </form>
       <div className="img-container">
-        {memeImg && <img src={memeImg} alt="meme img" className="form--img" />}
+        {meme.imgUrl && (
+          <img src={meme.imgUrl} alt="meme img" className="form--img" />
+        )}
+        {meme.topText && <h2 className="meme-txt top">{meme.topText}</h2>}
+        {meme.bottomText && (
+          <h2 className="meme-txt bottom">{meme.bottomText}</h2>
+        )}
       </div>
     </main>
   );
